6a056e4995610fd88b8ec64ee429b90ceae14530
Mateus C. <mateus.c000@outlook.com>
2024-09-15 03:11:49 -0300

    Finalizando o Crud de usuário

--- Back/Ecommerce/src/main/java/com/Ecommerce/Ecommerce/controller/UsuarioController.java
+++ Back/Ecommerce/src/main/java/com/Ecommerce/Ecommerce/controller/UsuarioController.java
@@ -2,84 +2,54 @@ package com.Ecommerce.Ecommerce.controller;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.Ecommerce.Ecommerce.dto.UsuarioDTO;
-import com.Ecommerce.Ecommerce.entity.Endereco;
-import com.Ecommerce.Ecommerce.entity.Funcao;
-import com.Ecommerce.Ecommerce.entity.Usuario;
-import com.Ecommerce.Ecommerce.repository.EnderecoRepository;
-import com.Ecommerce.Ecommerce.repository.FuncaoRepository;
-import com.Ecommerce.Ecommerce.repository.UsuarioRepository;
-import com.Ecommerce.Ecommerce.util.UsuarioMapper;
+import com.Ecommerce.Ecommerce.service.UsuarioService;
 
-@Service
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
     @Autowired
-    private UsuarioRepository usuarioRepository;
+    private UsuarioService usuarioService;
 
-    @Autowired
-    private EnderecoRepository enderecoRepository;
+    @PostMapping
+    public UsuarioDTO criarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+        return usuarioService.criarUsuario(usuarioDTO);
 
-    @Autowired
-    private FuncaoRepository funcaoRepository;
-
-    public UsuarioDTO criarUsuario(UsuarioDTO usuarioDTO) {
-        // Converter DTO para entidade usuario
-        Usuario usuario = UsuarioMapper.toEntity(usuarioDTO);
-
-        // Buscar e associar o endereco usando o ID
-        if (usuario.getEndereco() != null && usuario.getEndereco().getId() != null) {
-            Optional<Endereco> enderecoOptional = enderecoRepository.findById(usuario.getEndereco().getId());
-            if (enderecoOptional.isPresent()) {
-                usuario.setEndereco(enderecoOptional.get());
-            } else {
-                throw new RuntimeException("Endereço com ID " + usuario.getEndereco().getId() + " não encontrado.");
-            }
-        }
-        if (usuario.getFuncao() != null && usuario.getFuncao().getId() != null) {
-            Optional<Funcao> funcaoOptional = funcaoRepository.findById(usuario.getFuncao().getId());
-            if (funcaoOptional.isPresent()) {
-                usuario.setFuncao(funcaoOptional.get());
-            } else {
-                throw new RuntimeException("Funcao com ID " + usuario.getFuncao().getId() + " não encontrado.");
-            }
-        }
-        usuario = usuarioRepository.save(usuario);
-        return UsuarioMapper.toDTO(usuario);
     }
 
-    public List<UsuarioDTO> obterTodosUsuarios() {
-        return usuarioRepository.findAll().stream()
-                .map(UsuarioMapper::toDTO)
-                .collect(Collectors.toList());
+    @GetMapping
+    public ResponseEntity<List<UsuarioDTO>> obterTodosUsuarios() {
+        List<UsuarioDTO> usuarios = usuarioService.obterTodosUsuarios();
+        return ResponseEntity.ok(usuarios);
     }
 
-    public UsuarioDTO obterUsuarioPorId(Long id) {
-        return usuarioRepository.findById(id)
-                .map(UsuarioMapper::toDTO)
-                .orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> obterUsuarioPorId(@PathVariable Long id) {
+        UsuarioDTO usuario = usuarioService.obterUsuarioPorId(id);
+        return ResponseEntity.ok(usuario);
     }
 
-    public UsuarioDTO atualizarUsuario(Long id, UsuarioDTO usuarioDTO) {
-        Usuario usuarioExistente = usuarioRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
-
-        usuarioExistente.setNome(usuarioDTO.getNome());
-        Usuario usuarioAtualizado = usuarioRepository.save(usuarioExistente);
-        return UsuarioMapper.toDTO(usuarioAtualizado);
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> atualizarUsuario(@PathVariable Long id, @RequestBody UsuarioDTO usuarioDTO) {
+        UsuarioDTO usuarioAtualizado = usuarioService.atualizarUsuario(id, usuarioDTO);
+        return ResponseEntity.ok(usuarioAtualizado);
     }
 
-    public boolean deletarUsuario(Long id) {
-        Optional<Usuario> usuarioExistente = usuarioRepository.findById(id);
-        if(usuarioExistente.isPresent()){
-            usuarioRepository.deleteById(id);
-            return true;
-        } else {
-            return false;
-        }
+    @SuppressWarnings("rawtypes")
+    @DeleteMapping("/{id}")
+    public Optional deletarUsuario(@PathVariable Long id) {
+        return Optional.ofNullable(usuarioService.deletarUsuario(id));
     }
 }
--- Back/Ecommerce/src/main/java/com/Ecommerce/Ecommerce/service/UsuarioService.java
+++ Back/Ecommerce/src/main/java/com/Ecommerce/Ecommerce/service/UsuarioService.java
@@ -1,5 +1,85 @@
 package com.Ecommerce.Ecommerce.service;
 
-public class UsuarioService {
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Ecommerce.Ecommerce.dto.UsuarioDTO;
+import com.Ecommerce.Ecommerce.entity.Endereco;
+import com.Ecommerce.Ecommerce.entity.Funcao;
+import com.Ecommerce.Ecommerce.entity.Usuario;
+import com.Ecommerce.Ecommerce.repository.EnderecoRepository;
+import com.Ecommerce.Ecommerce.repository.FuncaoRepository;
+import com.Ecommerce.Ecommerce.repository.UsuarioRepository;
+import com.Ecommerce.Ecommerce.util.UsuarioMapper;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private FuncaoRepository funcaoRepository;
+
+    public UsuarioDTO criarUsuario(UsuarioDTO usuarioDTO) {
+        // Converter DTO para entidade usuario
+        Usuario usuario = UsuarioMapper.toEntity(usuarioDTO);
+
+        // Buscar e associar o endereco usando o ID
+        if (usuario.getEndereco() != null && usuario.getEndereco().getId() != null) {
+            Optional<Endereco> enderecoOptional = enderecoRepository.findById(usuario.getEndereco().getId());
+            if (enderecoOptional.isPresent()) {
+                usuario.setEndereco(enderecoOptional.get());
+            } else {
+                throw new RuntimeException("Endereço com ID " + usuario.getEndereco().getId() + " não encontrado.");
+            }
+        }
+        if (usuario.getFuncao() != null && usuario.getFuncao().getId() != null) {
+            Optional<Funcao> funcaoOptional = funcaoRepository.findById(usuario.getFuncao().getId());
+            if (funcaoOptional.isPresent()) {
+                usuario.setFuncao(funcaoOptional.get());
+            } else {
+                throw new RuntimeException("Funcao com ID " + usuario.getFuncao().getId() + " não encontrado.");
+            }
+        }
+        usuario = usuarioRepository.save(usuario);
+        return UsuarioMapper.toDTO(usuario);
+    }
+
+    public List<UsuarioDTO> obterTodosUsuarios() {
+        return usuarioRepository.findAll().stream()
+                .map(UsuarioMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public UsuarioDTO obterUsuarioPorId(Long id) {
+        return usuarioRepository.findById(id)
+                .map(UsuarioMapper::toDTO)
+                .orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+    }
+
+    public UsuarioDTO atualizarUsuario(Long id, UsuarioDTO usuarioDTO) {
+        Usuario usuarioExistente = usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+
+        usuarioExistente.setNome(usuarioDTO.getNome());
+        Usuario usuarioAtualizado = usuarioRepository.save(usuarioExistente);
+        return UsuarioMapper.toDTO(usuarioAtualizado);
+    }
+
+    public boolean deletarUsuario(Long id) {
+        Optional<Usuario> usuarioExistente = usuarioRepository.findById(id);
+        if(usuarioExistente.isPresent()){
+            usuarioRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
