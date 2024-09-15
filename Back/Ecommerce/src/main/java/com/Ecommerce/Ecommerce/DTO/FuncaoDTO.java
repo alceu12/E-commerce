a1f40f6e6940fdf45bfb669f1f25a9cb24dc639e
Mateus C. <mateus.c000@outlook.com>
2024-09-15 01:27:24 -0300

    Criando as entidades Funcao e FuncaoDTO

---
+++ Back/Ecommerce/src/main/java/com/Ecommerce/Ecommerce/DTO/FuncaoDTO.java
@@ -0,0 +1,30 @@
+package com.Ecommerce.Ecommerce.DTO;
+
+public class FuncaoDTO {
+
+    private Long id;
+    private String nome;
+    
+    public FuncaoDTO() {
+    }
+
+    public FuncaoDTO( String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
