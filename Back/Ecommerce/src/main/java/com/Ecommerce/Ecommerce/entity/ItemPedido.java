bb7490c194043789fbba10a81683e3db6a13b0e5
Mateus C <mateus.c000@outlook.com>
2024-09-30 19:12:16 -0300

    correção de bug na entidade itemPedido

--- Back/Ecommerce/src/main/java/com/Ecommerce/Ecommerce/entity/ItemPedido.java
+++ Back/Ecommerce/src/main/java/com/Ecommerce/Ecommerce/entity/ItemPedido.java
@@ -2,6 +2,9 @@ package com.Ecommerce.Ecommerce.entity;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
@@ -14,6 +17,8 @@ import lombok.NoArgsConstructor;
 @Entity
 public class ItemPedido {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int quantidade;
     private double precoUnitario;
