a33aaabe05becd39d433a65ba6ac39e165f0d005
Mateus C <mateus.c000@outlook.com>
2024-09-30 21:24:33 -0300

    Finalizando o Crud de Pedido

--- Back/Ecommerce/src/main/java/com/Ecommerce/Ecommerce/DTO/PedidoDTO.java
+++ Back/Ecommerce/src/main/java/com/Ecommerce/Ecommerce/DTO/PedidoDTO.java
@@ -1,5 +1,7 @@
 package com.Ecommerce.Ecommerce.dto;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +10,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PedidoDTO {
+
     private Long id;
     private double total;
     private StatusDTO statusDTO;
-    private ItemPedidoDTO itemPedidoDTO;
+    private List<ItemPedidoDTO> itemPedidoDTO;
     private UsuarioDTO usuarioDTO;
+    
 }
