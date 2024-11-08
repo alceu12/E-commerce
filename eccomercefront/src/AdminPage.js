4d0f91b98b8b7a2f003bfd381f058f91cb9e94d3
alceu12 <55637357+alceu12@users.noreply.github.com>
2024-11-07 22:07:11 -0300

    Feat: ajuste front para imagem

--- eccomercefront/src/AdminPage.js
+++ eccomercefront/src/AdminPage.js
@@ -1,31 +1,30 @@
-import React, { useState } from "react";
+import React from "react"; 
 import { Box, Container } from "@mui/material";
 import Sidebar from "./componentes/AdminPage/Sidebar";
-import Product from "./componentes/AdminPage/Produto";
-import Category from "./componentes/AdminPage/Categoria";
-import Coupon from "./componentes/AdminPage/Cupom";
+import Produto from "./componentes/AdminPage/Produto";
+import Categoria from "./componentes/AdminPage/Categoria";
+import Cupom from "./componentes/AdminPage/Cupom";
+import ProdutoDetalhes from "./componentes/AdminPage/ProdutoDetalhes";
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 const AdminPage = () => {
-  const [selectedSection, setSelectedSection] = useState("products");
+  const navigate = useNavigate();
 
-  const renderContent = () => {
-    switch (selectedSection) {
-      case "products":
-        return <Product />;
-      case "categories":
-        return <Category />;
-      case "coupons":
-        return <Coupon />;
-      default:
-        return <Product />;
-    }
+  const handleSectionSelect = (section) => {
+    navigate(`/admin/${section}`);
   };
 
   return (
     <Box sx={{ display: "flex" }}>
-      <Sidebar onSelect={setSelectedSection} />
+      <Sidebar onSelect={handleSectionSelect} />
       <Container sx={{ mt: 4, ml: 3, flexGrow: 1 }}>
-        {renderContent()}
+        <Routes>
+          <Route path="products" element={<Produto />} />
+          <Route path="products/:id" element={<ProdutoDetalhes />} />
+          <Route path="categories" element={<Categoria />} />
+          <Route path="coupons" element={<Cupom />} />
+          <Route path="*" element={<Produto />} />
+        </Routes>
       </Container>
     </Box>
   );
--- eccomercefront/src/App.js
+++ eccomercefront/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-import Homecliente from './Homecliente';
 import AdminPage from './AdminPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 function App() {
   return (
-    <div className="App">
-      <Homecliente />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/admin/*" element={<AdminPage />} />
+      </Routes>
+    </Router>
   );
 }
 
