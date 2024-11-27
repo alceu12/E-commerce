b49f5e72d3e93951f07233e4e3464cc29aafa8ed
alceu12 <55637357+alceu12@users.noreply.github.com>
2024-11-27 19:39:33 -0300

    Update Api.js

--- eccomercefront/src/Service/Api.js
+++ eccomercefront/src/Service/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import qs from 'qs';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://e-commerce-07vh.onrender.com/',
   paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }),
 });
 
